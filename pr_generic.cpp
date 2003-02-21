4fffa8f2e0817a8e2e2a47c7da5e9e9c886ad013
Moritz Bunkus <moritz@bunkus.org>
2003-02-21 09:26:40 +0000

    some generic functions for the readers/packetizers

---
+++ pr_generic.cpp
@@ -0,0 +1,52 @@
+/*
+  mkvmerge -- utility for splicing together matroska files
+      from component media subtypes
+
+  pr_generic.cpp
+
+  Written by Moritz Bunkus <moritz@bunkus.org>
+
+  Distributed under the GPL
+  see the file COPYING for details
+  or visit http://www.gnu.org/copyleft/gpl.html
+*/
+
+/*!
+    \file
+    \version \$Id: pr_generic.cpp,v 1.1 2003/02/21 09:26:40 mosu Exp $
+    \brief functions common for all readers/packetizers
+    \author Moritz Bunkus         <moritz @ bunkus.org>
+*/
+
+#include <malloc.h>
+
+#include "pr_generic.h"
+
+generic_packetizer_c::generic_packetizer_c() {
+  track_entry = NULL;
+  private_data = NULL;
+  private_data_size = 0;
+}
+
+generic_packetizer_c::~generic_packetizer_c() {
+  if (private_data != NULL)
+    free(private_data);
+}
+
+void generic_packetizer_c::set_private_data(void *data, int size) {
+  if (private_data != NULL)
+    free(private_data);
+  private_data = malloc(size);
+  if (private_data == NULL)
+    die("malloc");
+  memcpy(private_data, data, size);
+  private_data_size = size;
+}
+
+//--------------------------------------------------------------------
+
+generic_reader_c::generic_reader_c() {
+}
+
+generic_reader_c::~generic_reader_c() {
+}
