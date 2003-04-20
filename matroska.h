33899462b33bebab696af53bfe4cdf549433675f
Moritz Bunkus <moritz@bunkus.org>
2003-04-20 21:05:42 +0000

    The specs changed A_DOL_AC3 to A_AC3.

--- matroska.h
+++ matroska.h
@@ -13,7 +13,7 @@
 
 /*!
     \file
-    \version \$Id: matroska.h,v 1.2 2003/04/20 20:08:02 mosu Exp $
+    \version \$Id: matroska.h,v 1.3 2003/04/20 21:05:42 mosu Exp $
     \brief Definitions for the various Codec IDs
     \author Moritz Bunkus         <moritz @ bunkus.org>
 */
@@ -24,7 +24,7 @@
 // see http://cvs.sourceforge.net/cgi-bin/viewcvs.cgi/*checkout*/matroska/doc/website/specs/codex.html?rev=HEAD&content-type=text/html
 
 #define MKV_A_MP3        "A_MPEGLAYER3"
-#define MKV_A_AC3        "A_DOL_AC3"
+#define MKV_A_AC3        "A_AC3"
 #define MKV_A_PCM16      "A_PCM16IN"
 #define MKV_A_VORBIS     "A_VORBIS"
 #define MKV_A_ACM        "A_MS/ACM"
