202b6f813b7029311d5109031883e272c8cbc2a3
Moritz Bunkus <moritz@bunkus.org>
2004-03-01 09:51:30 +0000

    Get rid of automake.

--- autogen.sh
+++ autogen.sh
@@ -52,38 +52,38 @@ cd "$srcdir"
 DIE=0
 
 (autoconf --version) < /dev/null > /dev/null 2>&1 || {
-        echo
-        echo "You must have autoconf installed to compile $package."
-        echo "Download the appropriate package for your distribution,"
-        echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
-        DIE=1
+  echo
+  echo "You must have autoconf installed to compile $package."
+  echo "Download the appropriate package for your distribution,"
+  echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+  DIE=1
 }
 
 (autoheader --version) < /dev/null > /dev/null 2>&1 || {
-        echo
-        echo "You must have autoheader installed to compile $package."
-        echo "Download the appropriate package for your distribution,"
-        echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
-        DIE=1
+  echo
+  echo "You must have autoheader installed to compile $package."
+  echo "Download the appropriate package for your distribution,"
+  echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+  DIE=1
 }
 
-(automake --version) < /dev/null > /dev/null 2>&1 || {
-        echo
-        echo "You must have automake installed to compile $package."
-	echo "Download the appropriate package for your system,
-	echo "or get the source from one of the GNU ftp sites"
-	echo "listed in http://www.gnu.org/order/ftp.html"
-        DIE=1
-}
+# (automake --version) < /dev/null > /dev/null 2>&1 || {
+#   echo
+#   echo "You must have automake installed to compile $package."
+#   echo "Download the appropriate package for your system,"
+#   echo "or get the source from one of the GNU ftp sites"
+#   echo "listed in http://www.gnu.org/order/ftp.html"
+#   DIE=1
+# }
 
-0 && (libtool --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have libtool installed to compile $package."
-	echo "Download the appropriate package for your system,
-	echo "or get the source from one of the GNU ftp sites"
-	echo "listed in http://www.gnu.org/order/ftp.html"
-	DIE=1
-}
+# (libtool --version) < /dev/null > /dev/null 2>&1 || {
+#   echo
+#   echo "You must have libtool installed to compile $package."
+#   echo "Download the appropriate package for your system,"
+#   echo "or get the source from one of the GNU ftp sites"
+#   echo "listed in http://www.gnu.org/order/ftp.html"
+#   DIE=1
+# }
 
 if test "$DIE" -eq 1; then
         exit 1
@@ -104,10 +104,13 @@ echo "Generating configuration files for $package, please wait...."
 
 rm -f config.h config.h.in stamp-h1 &> /dev/null
 
-echo "  aclocal $ACLOCAL_FLAGS" && aclocal $ACLOCAL_FLAGS
+#echo "  aclocal $ACLOCAL_FLAGS" && aclocal $ACLOCAL_FLAGS
 echo "  autoheader" && autoheader
 #echo "  libtoolize --automake" && libtoolize --automake
 echo "  autoconf" && autoconf
-echo "  automake --add-missing --copy" && automake --add-missing --copy
+#echo "  automake --add-missing --copy" && automake --add-missing --copy
 
+echo
+echo "You can run './configure' now. If you need dependencies then"
+echo "run 'make depend' afterwards."
 cd $olddir
