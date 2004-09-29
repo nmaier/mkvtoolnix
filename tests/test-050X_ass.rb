ed8b56e85822d872e4956179e782c872c046446c
Moritz Bunkus <moritz@bunkus.org>
2004-09-29 09:40:08 +0000

    New tests for ASS muxing/demuxing.

---
+++ tests/test-050X_ass.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/ruby -w
+
+class T_050X_ass < Test
+  def description
+    return "mkvextract / ASS subtitles / out(ASS)"
+  end
+
+  def run
+    my_hash = hash_file("data/textsubs/11.Magyar.ass")
+    my_tmp = tmp
+    merge("--sub-charset 0:ISO-8859-1 data/textsubs/11.Magyar.ass")
+    @tmp = nil
+    xtr_tracks(my_tmp, "-c ISO-8859-1 1:#{tmp}")
+    File.unlink(my_tmp)
+    return my_hash + "-" + hash_tmp
+  end
+end
+
