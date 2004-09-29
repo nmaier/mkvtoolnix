84e25ea39f17013865ec9a70a06b65863830ddc5
Moritz Bunkus <moritz@bunkus.org>
2004-09-29 09:41:22 +0000

    Merged 2239

---
+++ tests/test-049ass.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby -w
+
+class T_049ass < Test
+  def description
+    return "mkvmerge / ASS subtitles / in(ASS)"
+  end
+
+  def run
+    merge("--sub-charset 0:ISO-8859-1 data/textsubs/11.Magyar.ass")
+    return hash_tmp
+  end
+end
+
