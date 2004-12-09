32dcc828f06514adaed787af5eee0a8de5132ebc
Moritz Bunkus <moritz@bunkus.org>
2004-12-09 12:46:48 +0000

    Merged 2549 from the 1.0 branch.

--- src/mmg/tab_input.cpp
+++ src/mmg/tab_input.cpp
@@ -1414,6 +1414,8 @@ tab_input::load(wxConfigBase *cfg) {
   int num_files, num_tracks;
   bool b;
 
+  dont_copy_values_now = true;
+
   clb_tracks->Clear();
   lb_input_files->Clear();
   no_track_mode();
@@ -1450,8 +1452,10 @@ tab_input::load(wxConfigBase *cfg) {
   tracks.clear();
 
   cfg->SetPath(wxT("/input"));
-  if (!cfg->Read(wxT("number_of_files"), &num_files) || (num_files < 0))
+  if (!cfg->Read(wxT("number_of_files"), &num_files) || (num_files < 0)) {
+    dont_copy_values_now = false;
     return;
+  }
 
   fix_format("%u:%lld", format);
   for (fidx = 0; fidx < num_files; fidx++) {
@@ -1608,6 +1612,8 @@ tab_input::load(wxConfigBase *cfg) {
   }
   st_tracks->Enable(tracks.size() > 0);
   clb_tracks->Enable(tracks.size() > 0);
+
+  dont_copy_values_now = false;
 }
 
 bool
