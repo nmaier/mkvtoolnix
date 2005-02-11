579270a9b8decb5e3362e877203e21364c731696
Moritz Bunkus <moritz@bunkus.org>
2005-02-11 22:20:18 +0000

    Added a test for VobSub extraction.

---
+++ tests/test-206X_vobsub.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/ruby -w
+
+class T_206X_vobsub < Test
+  def description
+    return "mkvextract / VobSubs / in(MKV)"
+  end
+
+  def run
+    xtr_tracks("data/mkv/vobsubs.mks", "4:#{tmp}")
+    hash = hash_file("#{tmp}.idx") + "-" + hash_file("#{tmp}.sub")
+    arg = ""
+    1.upto(7) { |i| arg += "#{i}:#{tmp} " }
+    xtr_tracks("data/mkv/vobsubs.mks", arg)
+    hash += "-" + hash_file("#{tmp}.idx") + "-" + hash_file("#{tmp}.sub")
+    merge("#{tmp}.idx")
+    hash += "-" + hash_tmp
+    unlink_tmp_files
+    return hash
+  end
+end
+
