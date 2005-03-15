3a2fb8538a4690b2fd62c727c017a2e5e00bbaaf
Moritz Bunkus <moritz@bunkus.org>
2005-03-15 08:30:03 +0000

    Added a test for a formerly mis-detected AC3 file.

---
+++ tests/test-209ac3misdeetected_as_mpeges.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby -w
+
+class T_209ac3misdeetected_as_mpeges < Test
+  def description
+    return "mkvmerge / AC3 misdetected as MPEG-ES / in(AC3)"
+  end
+
+  def run
+    merge("data/simple/misdetected_as_mpeges.ac3")
+    return hash_tmp
+  end
+end
+
