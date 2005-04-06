223e1a261396785d05ef49e1a0c641cdeb739c53
Moritz Bunkus <moritz@bunkus.org>
2005-04-06 14:52:15 +0000

    Added a test for the chapter handling during splitting (see commit 2869 and Anthill bug 122).

---
+++ tests/test-210splitting_and_chapters.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/ruby -w
+
+class T_210splitting_and_chapters < Test
+  def description
+    return "mkvmerge / splitting and chapters / in(AVI)"
+  end
+
+  def run
+    merge(tmp + "-%03d ", "--split-max-files 2 --split 4m data/avi/v.avi " +
+           "--chapters data/text/shortchaps.txt")
+    if (!FileTest.exist?(tmp + "-001"))
+      error("First split file does not exist.")
+    end
+    if (!FileTest.exist?(tmp + "-002"))
+      File.unlink(tmp + "-001")
+      error("Second split file does not exist.")
+    end
+    hash = hash_file(tmp + "-001") + "-" + hash_file(tmp + "-002")
+    File.unlink(tmp + "-001")
+    File.unlink(tmp + "-002")
+
+    merge(tmp + "-%03d ", "--split-max-files 2 --split 4m data/avi/v.avi " +
+           "--chapters data/text/shortchaps.txt --link")
+    if (!FileTest.exist?(tmp + "-001"))
+      error("First split file does not exist.")
+    end
+    if (!FileTest.exist?(tmp + "-002"))
+      File.unlink(tmp + "-001")
+      error("Second split file does not exist.")
+    end
+    hash += "-" + hash_file(tmp + "-001") + "-" + hash_file(tmp + "-002")
+    File.unlink(tmp + "-001")
+    File.unlink(tmp + "-002")
+
+    return hash
+  end
+end
+
