b8296e4d0bdf8fa917dd1fa0071f0f6f3134fe1d
Moritz Bunkus <moritz@bunkus.org>
2005-07-01 08:11:31 +0000

    Updated the version number & the release code name.

--- src/common/common.h
+++ src/common/common.h
@@ -55,7 +55,7 @@ namespace libebml {
 
 using namespace libebml;
 
-#define VERSIONNAME "Jimi Thing"
+#define VERSIONNAME "It's alright, baby"
 #define VERSIONINFO "mkvmerge v" VERSION " ('" VERSIONNAME "')"
 #define BUGMSG _("This should not have happened. Please contact the author " \
                  "Moritz Bunkus <moritz@bunkus.org> with this error/warning " \
