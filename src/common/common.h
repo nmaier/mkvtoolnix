5a0b3ac802321682956e9a9641aba4dbdbb6ec15
Moritz Bunkus <moritz@bunkus.org>
2005-10-05 09:40:22 +0000

    Bumped the version number to 1.5.7 and set the version name.

--- src/common/common.h
+++ src/common/common.h
@@ -55,7 +55,7 @@ namespace libebml {
 
 using namespace libebml;
 
-#define VERSIONNAME "Breathe me"
+#define VERSIONNAME "Ist das so"
 #define VERSIONINFO "mkvmerge v" VERSION " ('" VERSIONNAME "')"
 #define BUGMSG _("This should not have happened. Please contact the author " \
                  "Moritz Bunkus <moritz@bunkus.org> with this error/warning " \
