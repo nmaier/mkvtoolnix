69fec722470354cb9e3d521f405b63cd22abdcc2
Moritz Bunkus <moritz@bunkus.org>
2006-11-13 22:47:13 +0000

    Do not create temp files.

--- tests/test-205X_cuesheets.rb
+++ tests/test-205X_cuesheets.rb
@@ -11,7 +11,7 @@ class T_205X_cuesheets < Test
     sys("../src/mkvextract cuesheet #{tmp}-src --no-variable-data &> #{tmp}")
     hash = hash_tmp(false)
     sys("../src/mkvextract tracks #{tmp}-src --no-variable-data " +
-         "--cuesheet 1:#{tmp} > /tmp/a")
+         "--cuesheet 1:#{tmp} > /dev/null")
     hash += "-" + hash_tmp("#{tmp}.cue")
     unlink_tmp_files
     return hash
