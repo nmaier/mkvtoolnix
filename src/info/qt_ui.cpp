5f42409744853f49ac8bfaeddcd3f0d918c4df14
Moritz Bunkus <moritz@bunkus.org>
2006-12-08 16:29:26 +0000

    A fix for compilation with Qt < 4.2.0.

--- src/info/qt_ui.cpp
+++ src/info/qt_ui.cpp
@@ -242,8 +242,13 @@ rightclick_tree_widget::mousePressEvent(QMouseEvent *event) {
   }
 
   QTreeWidgetItem *item = itemAt(event->pos());
-  if (NULL != item)
+  if (NULL != item) {
+#if QT_VERSION >= 0x040200
     gui->expand_all_elements(item, !item->isExpanded());
+#else   // QT_VERSION >= 0x040200
+    gui->expand_all_elements(item, !item->treeWidget()->isItemExpanded(item));
+#endif  // QT_VERSION >= 0x040200
+  }
 }
 
 void
