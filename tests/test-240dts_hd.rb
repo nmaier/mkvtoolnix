a1962191eeccc0fede7a16793e436bcefcb9c8fc
Moritz Bunkus <moritz@bunkus.org>
2008-03-09 16:08:02 +0000

    Added a test for DTS-HD.

---
+++ tests/test-240dts_hd.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/ruby -w
+
+class T_240dts_hd < Test
+  def description
+    return "mkvmerge / DTS-HD / in(*)"
+  end
+
+  def run
+    hashes = Array.new
+
+    merge("data/simple/dts-hd.dts")
+    hashes << hash_tmp
+
+    merge("-D data/vob/VC1WithDTSHD.EVO")
+    hashes << hash_tmp
+
+    return hashes.join("-")
+  end
+end
+
