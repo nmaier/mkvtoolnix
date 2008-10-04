cf80ea72d567ca543836eca03f4b9a71c007ea69
Moritz Bunkus <moritz@bunkus.org>
2008-10-04 19:38:07 +0000

    New test for iconv Hebrew handling.

---
+++ tests/test-244iconv_missing_character.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby -w
+
+class T_244iconv_missing_character < Test
+  def description
+    return "mkvmerge / iconv cuts the last character (e.g. Hebrew) / in(SRT)"
+  end
+
+  def run
+    merge("--sub-charset -1:CP1255 data/textsubs/Madagascar.Hebrew.Encoding.srt")
+    return hash_tmp
+  end
+end
+
