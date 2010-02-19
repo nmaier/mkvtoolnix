7e399edf91b610415f98b7432c9aac82174212a2
Moritz Bunkus <moritz@bunkus.org>
2010-02-19 16:58:08 +0100

    Handle h264 ES streams with CodecID V_MPEG4/ISO/AVC

--- src/input/r_matroska.cpp
+++ src/input/r_matroska.cpp
@@ -1617,8 +1617,11 @@ kax_reader_c::parse_first_mpeg4_p10_frame(kax_track_t *t,
 
     if (sizeof(alBITMAPINFOHEADER) < t->private_size)
       parser->add_bytes((unsigned char *)t->private_data + sizeof(alBITMAPINFOHEADER), t->private_size - sizeof(alBITMAPINFOHEADER));
-    foreach(memory_cptr &frame, t->first_frames_data)
+    foreach(memory_cptr &frame, t->first_frames_data) {
       parser->add_bytes(frame->get_buffer(), frame->get_size());
+      mxinfo("---\n");
+      mxhexdump(0, frame->get_buffer(), std::min(static_cast<int>(frame->get_size()), 64));
+    }
     parser->flush();
 
     if (!parser->headers_parsed())
@@ -1653,13 +1656,8 @@ kax_reader_c::create_mpeg4_p10_video_packetizer(kax_track_t *t,
                                                 track_info_c &nti) {
   if ((0 == nti.private_size) || (NULL == nti.private_data)) {
     avc_es_parser_cptr parser = parse_first_mpeg4_p10_frame(t, nti);
-    memory_cptr avcc          = parser->get_avcc();
-
-    safefree(nti.private_data);
-    nti.private_data = static_cast<unsigned char *>(safememdup(avcc->get_buffer(), avcc->get_size()));
-    nti.private_size = avcc->get_size();
-
-    mxverb_tid(2, ti.fname, t->tnum, boost::format("AVC track missing CodecPrivate; re-created from bitstream with size %1%\n") % nti.private_size);
+    create_mpeg4_p10_es_video_packetizer(t, nti);
+    return;
   }
 
   if (verbose)
