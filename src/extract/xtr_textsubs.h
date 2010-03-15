64006ff2da06f54d9c94b82fb5704905d7b79fcf
Steve Lhomme <robux4@gmail.com>
2010-03-15 10:22:48 +0100

    the common/element_writer.h implies common.h

--- src/extract/xtr_textsubs.h
+++ src/extract/xtr_textsubs.h
@@ -13,10 +13,7 @@
 #ifndef __XTR_TEXTSUBS_H
 #define __XTR_TEXTSUBS_H
 
-#include "common/os.h"
-
-#include <string>
-#include <vector>
+#include "common/common.h"
 
 #include "common/xml/element_writer.h"
 #include "extract/xtr_base.h"
