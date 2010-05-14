fa867a58ffd9d3aa09ccba6d193f19fafa5f2f68
Moritz Bunkus <moritz@bunkus.org>
2010-05-14 19:56:01 +0200

    Swapped lines in cosmetics patch

--- src/common/translation.cpp
+++ src/common/translation.cpp
@@ -53,8 +53,8 @@ translation_c::translation_c(const std::string &unix_locale,
 void
 translation_c::initialize_available_translations() {
   ms_available_translations.clear();
-#if defined(HAVE_LIBINTL_H)
   ms_available_translations.push_back(translation_c("en_US", "en",    "english",   "English",             "English",     false));
+#if defined(HAVE_LIBINTL_H)
   ms_available_translations.push_back(translation_c("de_DE", "de",    "german",    "German",              "Deutsch",     false));
   ms_available_translations.push_back(translation_c("ja_JP", "ja",    "japanese",  "Japanese",            "日本語",      true));
   ms_available_translations.push_back(translation_c("ru_RU", "ru",    "russian",   "Russian",             "Русский",     false));
