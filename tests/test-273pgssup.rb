7f6c628ccf557bc71a8a1b580b28782467251d9e
Moritz Bunkus <moritz@bunkus.org>
2010-06-18 12:24:21 +0200

    Test case for PGS/SUP files

---
+++ tests/test-273pgssup.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/ruby -w
+
+class T_273pgssup < Test
+  def description
+    return "mkvmerge & mkvextract / PGS SUP files"
+  end
+
+  def run
+    hashes = Array.new
+    merge "data/subtitles/pgs/x.sup"
+    hashes << hash_tmp
+
+    merge "#{tmp}-1", "data/subtitles/pgs/x.sup"
+    merge "#{tmp}-2", "#{tmp}-1"
+    hashes << hash_file("#{tmp}-2")
+
+    xtr_tracks "#{tmp}-1", "1:#{tmp}-3"
+    hashes << hash_file("#{tmp}-3")
+
+    unlink_tmp_files
+
+    return hashes.join "-"
+  end
+end
+
