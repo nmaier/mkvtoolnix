5e343448356ebd0d2c124d7109312e28910bf52d
Moritz Bunkus <moritz@bunkus.org>
2010-06-30 20:01:29 +0200

    Version number bump, release message, release code name

--- src/common/version.cpp
+++ src/common/version.cpp
@@ -18,7 +18,7 @@
 #include "common/strings/parsing.h"
 #include "common/version.h"
 
-#define VERSIONNAME "The Stars were mine"
+#define VERSIONNAME "Boiling Point"
 
 std::string
 get_version_info(const std::string &program,
