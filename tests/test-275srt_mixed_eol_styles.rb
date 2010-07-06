21cfbcc1c57322806588b4d084bf705e4b407c11
Moritz Bunkus <moritz@bunkus.org>
2010-07-06 09:09:19 +0200

    Test case for text files with mixed end-of-line markings

---
+++ tests/test-275srt_mixed_eol_styles.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby -w
+
+class T_275srt_mixed_eol_styles < Test
+  def description
+    return "mkvmerge / SRT files with mixed end-of-line markings"
+  end
+
+  def run
+    merge "'data/textsubs/Space Buddies (2009) AVCHD 1080p DTS.srt'"
+    return hash_tmp
+  end
+end
+
