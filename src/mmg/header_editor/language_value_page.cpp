1ee34768d63466c2c7055228141eba86e42e1b0c
Moritz Bunkus <moritz@bunkus.org>
2010-07-30 09:42:08 +0200

    Properly handle default value for 'track language' elements in the header editor
    Fix for bug 525.

--- src/mmg/header_editor/language_value_page.cpp
+++ src/mmg/header_editor/language_value_page.cpp
@@ -53,8 +53,7 @@ he_language_value_page_c::translate_ui() {
 
 wxControl *
 he_language_value_page_c::create_input_control() {
-  if (NULL != m_element)
-    m_original_value = wxU(dynamic_cast<EbmlString *>(m_element));
+  m_original_value = NULL != m_element ? wxU(dynamic_cast<EbmlString *>(m_element)) : wxU("eng");
 
   m_cb_language = new wxMTX_COMBOBOX_TYPE(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0, NULL, wxCB_DROPDOWN | wxCB_READONLY);
 
