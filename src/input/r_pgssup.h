nmaier/mkvtoolnix

commit 06fa821050f066acb7facfe69a12b40472b225aa

Fix timecodes in appended subtitle files
If subtitle files are appended to separate video files
(e.g. two AVI and two SRT files) then the subtitle timecodes of
the second and all following subtitle files were based on the last
timecode in the first subtitle file instead of the last timecode in
the first video file. Fix for bug 325.

This bug was introduced in c68a6dfc272c6f62fe5f0ba162f03a17c49c6085

src/merge/output_control.cpp
@@ -1597,7 +1597,7 @@ append_track(packetizer_t &ptzr,
   // But then again I don't expect that people will try to concatenate such
   // files if they've been split before.
   int64_t timecode_adjustment = dst_file.reader->max_timecode_seen;
-  if (APPEND_MODE_FILE_BASED == g_append_mode)
+  if ((track_subtitle != ptzr.packetizer->get_track_type()) && (APPEND_MODE_FILE_BASED == g_append_mode))
     // Intentionally left empty.
     ;
 

commit 0001133ec1d256be2135b465a0c8265c275de4d5

Fix timecodes in appended subtitle tracks from compound containers
If subtitle tracks are appended to video tracks from the same file
(e.g. two Matroska files) then the subtitle timecodes of the second
and all following subtitle files were based on the last timecode in
the first subtitle track instead of the whoe file's last timecode. Fix
for bug 620.

This bug was introduced in 06fa821.

src/input/r_pgssup.h
@@ -36,6 +36,9 @@ public:
   virtual void identify();
   virtual int get_progress();
   virtual void create_packetizer(int64_t id);
+  virtual bool is_simple_subtitle_container() {
+    return true;
+  }
 
   static int probe_file(mm_io_c *in, uint64_t size);
 };

src/input/r_srt.h
@@ -33,6 +33,9 @@ public:
   virtual void identify();
   virtual void create_packetizer(int64_t tid);
   virtual int get_progress();
+  virtual bool is_simple_subtitle_container() {
+    return true;
+  }
 
   static int probe_file(mm_text_io_c *io, uint64_t size);
 };

src/input/r_ssa.h
@@ -32,6 +32,9 @@ public:
   virtual void identify();
   virtual void create_packetizer(int64_t tid);
   virtual int get_progress();
+  virtual bool is_simple_subtitle_container() {
+    return true;
+  }
 
   static int probe_file(mm_text_io_c *io, uint64_t size);
 };

src/input/r_usf.h
@@ -75,6 +75,9 @@ public:
   virtual void create_packetizer(int64_t tid);
   virtual void create_packetizers();
   virtual int get_progress();
+  virtual bool is_simple_subtitle_container() {
+    return true;
+  }
 
   static int probe_file(mm_text_io_c *io, uint64_t size);
 

src/input/r_vobsub.h
@@ -77,6 +77,9 @@ public:
   virtual void create_packetizer(int64_t tid);
   virtual void add_available_track_ids();
   virtual int get_progress();
+  virtual bool is_simple_subtitle_container() {
+    return true;
+  }
 
   static int probe_file(mm_io_c *io, uint64_t size);
 

src/merge/output_control.cpp
@@ -1706,7 +1706,9 @@ append_track(packetizer_t &ptzr,
   // But then again I don't expect that people will try to concatenate such
   // files if they've been split before.
   int64_t timecode_adjustment = dst_file.reader->m_max_timecode_seen;
-  if ((track_subtitle != ptzr.packetizer->get_track_type()) && (APPEND_MODE_FILE_BASED == g_append_mode))
+  if ((APPEND_MODE_FILE_BASED == g_append_mode)
+      && (   (track_subtitle != ptzr.packetizer->get_track_type())
+          || !dst_file.reader->is_simple_subtitle_container()))
     // Intentionally left empty.
     ;
 

src/merge/pr_generic.h
@@ -327,6 +327,9 @@ public:
   virtual void add_available_track_ids();
 
   virtual int64_t get_queued_bytes();
+  virtual bool is_simple_subtitle_container() {
+    return false;
+  }
 
   virtual file_status_e flush_packetizer(int num);
   virtual file_status_e flush_packetizer(generic_packetizer_c *ptzr);