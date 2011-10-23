nmaier/mkvtoolnix

commit eb846ed82e7d7c93b1e5de009ff784e68a0084b6

Build system: use pkg-config's own macros for library detection where possible

ac/curl.m4
@@ -2,36 +2,7 @@ dnl
 dnl Check for libcurl
 dnl
 
-continue_curl_check=1
-
-AC_ARG_WITH(curl_config,
-  AC_HELP_STRING([--with-curl-config=prog],[use prog instead of looking for curl-config]),
-  [ CURL_CONFIG="$with_curl_config" ],)
-AC_PATH_PROG(CURL_CONFIG, curl-config, no, $PATH:/usr/local/bin)
-if test x"$CURL_CONFIG" = "xno" ; then
-  echo "*** Not checking for curl: curl-config not found"
-  continue_curl_check=0
-elif test ! -x "$CURL_CONFIG" ; then
-  echo "*** Not checking for curl: file '"$CURL_CONFIG"' not executable"
-  continue_curl_check=0
-fi
-
-if test x"$continue_curl_check" = x1 ; then
-  CURL_CFLAGS=`$CURL_CONFIG --cflags`
-  CURL_LIBS=`$CURL_CONFIG --libs`
-  ac_save_CXXFLAGS="$CXXFLAGS"
-  ac_save_LIBS="$LIBS"
-  CXXFLAGS="$CXXFLAGS $CURL_CFLAGS"
-  LIBS="$LIBS $CURL_LIBS"
-  AC_CHECK_LIB(curl, curl_easy_init,
-               [ curl_found=yes ],
-               [ curl_found=no ],)
-  if test "$curl_found" = "yes"; then
-    AC_CHECK_HEADERS(curl/easy.h, , curl_found=no, [#include <curl/curl.h>])
-  fi
-  CXXFLAGS="$ac_save_CXXFLAGS"
-  LIBS="$ac_save_LIBS"
-fi
+PKG_CHECK_MODULES([CURL], [libcurl], [curl_found=yes])
 
 if test "$curl_found" = "yes"; then
   opt_features_yes="$opt_features_yes\n   * online update checks (via libcurl)"
@@ -40,6 +11,3 @@ else
   CURL_CFLAGS=""
   CURL_LIBS=""
 fi
-
-AC_SUBST(CURL_CFLAGS)
-AC_SUBST(CURL_LIBS)

ac/initialization.m4
@@ -13,6 +13,7 @@ AC_CHECK_TOOL(RANLIB, ranlib, :)
 AC_CHECK_TOOL(STRIP, strip, :)
 AC_CHECK_TOOL(AR, ar, :)
 AC_CHECK_TOOL(LD, ld, :)
+PKG_PROG_PKG_CONFIG
 
 dnl Check for headers
 AC_HEADER_STDC()

ac/pkg.m4
@@ -1,0 +1,159 @@
+# pkg.m4 - Macros to locate and utilise pkg-config.            -*- Autoconf -*-
+# serial 1 (pkg-config-0.24)
+# 
+# Copyright © 2004 Scott James Remnant <scott@netsplit.com>.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# As a special exception to the GNU General Public License, if you
+# distribute this file as part of a program that contains a
+# configuration script generated by Autoconf, you may include it under
+# the same distribution terms that you use for the rest of that program.
+
+# PKG_PROG_PKG_CONFIG([MIN-VERSION])
+# ----------------------------------
+AC_DEFUN([PKG_PROG_PKG_CONFIG],
+[m4_pattern_forbid([^_?PKG_[A-Z_]+$])
+m4_pattern_allow([^PKG_CONFIG(_(PATH|LIBDIR|SYSROOT_DIR|ALLOW_SYSTEM_(CFLAGS|LIBS)))?$])
+m4_pattern_allow([^PKG_CONFIG_(DISABLE_UNINSTALLED|TOP_BUILD_DIR|DEBUG_SPEW)$])
+AC_ARG_VAR([PKG_CONFIG], [path to pkg-config utility])
+AC_ARG_VAR([PKG_CONFIG_PATH], [directories to add to pkg-config's search path])
+AC_ARG_VAR([PKG_CONFIG_LIBDIR], [path overriding pkg-config's built-in search path])
+
+if test "x$ac_cv_env_PKG_CONFIG_set" != "xset"; then
+	AC_PATH_TOOL([PKG_CONFIG], [pkg-config])
+fi
+if test -n "$PKG_CONFIG"; then
+	_pkg_min_version=m4_default([$1], [0.9.0])
+	AC_MSG_CHECKING([pkg-config is at least version $_pkg_min_version])
+	if $PKG_CONFIG --atleast-pkgconfig-version $_pkg_min_version; then
+		AC_MSG_RESULT([yes])
+	else
+		AC_MSG_RESULT([no])
+		PKG_CONFIG=""
+	fi
+fi[]dnl
+])# PKG_PROG_PKG_CONFIG
+
+# PKG_CHECK_EXISTS(MODULES, [ACTION-IF-FOUND], [ACTION-IF-NOT-FOUND])
+#
+# Check to see whether a particular set of modules exists.  Similar
+# to PKG_CHECK_MODULES(), but does not set variables or print errors.
+#
+# Please remember that m4 expands AC_REQUIRE([PKG_PROG_PKG_CONFIG])
+# only at the first occurence in configure.ac, so if the first place
+# it's called might be skipped (such as if it is within an "if", you
+# have to call PKG_CHECK_EXISTS manually
+# --------------------------------------------------------------
+AC_DEFUN([PKG_CHECK_EXISTS],
+[AC_REQUIRE([PKG_PROG_PKG_CONFIG])dnl
+if test -n "$PKG_CONFIG" && \
+    AC_RUN_LOG([$PKG_CONFIG --exists --print-errors "$1"]); then
+  m4_default([$2], [:])
+m4_ifvaln([$3], [else
+  $3])dnl
+fi])
+
+# _PKG_CONFIG([VARIABLE], [COMMAND], [MODULES])
+# ---------------------------------------------
+m4_define([_PKG_CONFIG],
+[if test -n "$$1"; then
+    pkg_cv_[]$1="$$1"
+ elif test -n "$PKG_CONFIG"; then
+    PKG_CHECK_EXISTS([$3],
+                     [pkg_cv_[]$1=`$PKG_CONFIG --[]$2 "$3" 2>/dev/null`
+		      test "x$?" != "x0" && pkg_failed=yes ],
+		     [pkg_failed=yes])
+ else
+    pkg_failed=untried
+fi[]dnl
+])# _PKG_CONFIG
+
+# _PKG_SHORT_ERRORS_SUPPORTED
+# -----------------------------
+AC_DEFUN([_PKG_SHORT_ERRORS_SUPPORTED],
+[AC_REQUIRE([PKG_PROG_PKG_CONFIG])
+if $PKG_CONFIG --atleast-pkgconfig-version 0.20; then
+        _pkg_short_errors_supported=yes
+else
+        _pkg_short_errors_supported=no
+fi[]dnl
+])# _PKG_SHORT_ERRORS_SUPPORTED
+
+
+# PKG_CHECK_MODULES(VARIABLE-PREFIX, MODULES, [ACTION-IF-FOUND],
+# [ACTION-IF-NOT-FOUND])
+#
+#
+# Note that if there is a possibility the first call to
+# PKG_CHECK_MODULES might not happen, you should be sure to include an
+# explicit call to PKG_PROG_PKG_CONFIG in your configure.ac
+#
+#
+# --------------------------------------------------------------
+AC_DEFUN([PKG_CHECK_MODULES],
+[AC_REQUIRE([PKG_PROG_PKG_CONFIG])dnl
+AC_ARG_VAR([$1][_CFLAGS], [C compiler flags for $1, overriding pkg-config])dnl
+AC_ARG_VAR([$1][_LIBS], [linker flags for $1, overriding pkg-config])dnl
+
+pkg_failed=no
+AC_MSG_CHECKING([for $1])
+
+_PKG_CONFIG([$1][_CFLAGS], [cflags], [$2])
+_PKG_CONFIG([$1][_LIBS], [libs], [$2])
+
+m4_define([_PKG_TEXT], [Alternatively, you may set the environment variables $1[]_CFLAGS
+and $1[]_LIBS to avoid the need to call pkg-config.
+See the pkg-config man page for more details.])
+
+if test $pkg_failed = yes; then
+   	AC_MSG_RESULT([no])
+        _PKG_SHORT_ERRORS_SUPPORTED
+        if test $_pkg_short_errors_supported = yes; then
+	        $1[]_PKG_ERRORS=`$PKG_CONFIG --short-errors --print-errors --cflags --libs "$2" 2>&1`
+        else 
+	        $1[]_PKG_ERRORS=`$PKG_CONFIG --print-errors --cflags --libs "$2" 2>&1`
+        fi
+	# Put the nasty error message in config.log where it belongs
+	echo "$$1[]_PKG_ERRORS" >&AS_MESSAGE_LOG_FD
+
+	m4_default([$4], [AC_MSG_ERROR(
+[Package requirements ($2) were not met:
+
+$$1_PKG_ERRORS
+
+Consider adjusting the PKG_CONFIG_PATH environment variable if you
+installed software in a non-standard prefix.
+
+_PKG_TEXT])[]dnl
+        ])
+elif test $pkg_failed = untried; then
+     	AC_MSG_RESULT([no])
+	m4_default([$4], [AC_MSG_FAILURE(
+[The pkg-config script could not be found or is too old.  Make sure it
+is in your PATH or set the PKG_CONFIG environment variable to the full
+path to pkg-config.
+
+_PKG_TEXT
+
+To get pkg-config, see <http://pkg-config.freedesktop.org/>.])[]dnl
+        ])
+else
+	$1[]_CFLAGS=$pkg_cv_[]$1[]_CFLAGS
+	$1[]_LIBS=$pkg_cv_[]$1[]_LIBS
+        AC_MSG_RESULT([yes])
+	$3
+fi[]dnl
+])# PKG_CHECK_MODULES

ac/zlib.m4
@@ -1,13 +1,9 @@
 dnl
 dnl Check for zlib
 dnl
-  AC_CHECK_LIB(z, zlibVersion,
-               [ ZLIB_LIBS="-lz"
-                 zlib_found=yes ],
-               [ zlib_found=no ],)
-  if test "$zlib_found" = "no"; then
-    AC_MSG_ERROR([Could not find the zlib library])
-  fi
-  AC_CHECK_HEADERS(zlib.h, , zlib_found=no)
 
-AC_SUBST(ZLIB_LIBS)
+PKG_CHECK_MODULES([ZLIB],[zlib],[zlib_found=yes])
+
+if test x"$zlib_found" != xyes; then
+  AC_MSG_ERROR([Could not find the zlib library])
+fi

configure.in
@@ -12,6 +12,7 @@ dnl Optional features that are built and those that aren't
 opt_features_yes=""
 opt_features_no=""
 
+m4_include(ac/pkg.m4)
 m4_include(ac/initialization.m4)
 m4_include(ac/check_version.m4)
 m4_include(ac/gcc_version.m4)

commit e13f02d8b6d71c2846f5ec03bfeb27ea3fa57ad2

Build system: define HAVE_CURL_EASY_H it curl is found via pkg-config
Fix regression introduced in eb846ed8 when curl detection was changed
from AC_CHECK_HEADER() to pkg-config.

ac/curl.m4
@@ -6,6 +6,7 @@ PKG_CHECK_MODULES([CURL], [libcurl], [curl_found=yes])
 
 if test "$curl_found" = "yes"; then
   opt_features_yes="$opt_features_yes\n   * online update checks (via libcurl)"
+  AC_DEFINE(HAVE_CURL_EASY_H, 1, [define if libcurl is found via pkg-config])
 else
   opt_features_no="$opt_features_no\n   * online update checks (via libcurl)"
   CURL_CFLAGS=""