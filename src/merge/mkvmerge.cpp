bfcce8bd129d127553c74e0a81ba46d963ec7e0f
Moritz Bunkus <moritz@bunkus.org>
2011-12-28 15:18:18 +0100

    Don't list --check-for-updates in help even compiled without CURL

--- src/merge/mkvmerge.cpp
+++ src/merge/mkvmerge.cpp
@@ -251,7 +251,9 @@ set_usage() {
                   "                           the specified file (see man page).\n");
   usage_text += Y("  -h, --help               Show this help.\n");
   usage_text += Y("  -V, --version            Show version information.\n");
+#if defined(HAVE_CURL_EASY_H)
   usage_text += std::string("  --check-for-updates      ") + Y("Check online for the latest release.") + "\n";
+#endif
   usage_text +=   "\n\n";
   usage_text += Y("Please read the man page/the HTML documentation to mkvmerge. It\n"
                   "explains several details in great length which are not obvious from\n"
