nmaier/mkvtoolnix

commit 26565c724f90e06d3ebd31750eaeb3af4f8ad324

Don't tell DTS packetizer first frame is not available if it actually is
Fix for bug 705.

src/output/p_dts.cpp
@@ -24,13 +24,11 @@ using namespace libmatroska;
 
 dts_packetizer_c::dts_packetizer_c(generic_reader_c *p_reader,
                                    track_info_c &p_ti,
-                                   const dts_header_t &dtsheader,
-                                   bool get_first_header_later)
+                                   const dts_header_t &dtsheader)
   : generic_packetizer_c(p_reader, p_ti)
   , m_samples_written(0)
   , m_bytes_written(0)
   , m_packet_buffer(128 * 1024)
-  , m_get_first_header_later(get_first_header_later)
   , m_first_header(dtsheader)
   , m_previous_header(dtsheader)
   , m_skipping_is_normal(false)
@@ -63,21 +61,10 @@ dts_packetizer_c::get_dts_packet(dts_header_t &dtsheader) {
     buf_size = m_packet_buffer.get_size();
   }
 
-  pos = find_dts_header(buf, buf_size, &dtsheader, m_get_first_header_later ? false : !m_first_header.dts_hd);
+  pos = find_dts_header(buf, buf_size, &dtsheader, !m_first_header.dts_hd);
 
   if ((0 > pos) || (static_cast<int>(pos + dtsheader.frame_byte_size) > buf_size))
     return NULL;
-
-  if (m_get_first_header_later) {
-    m_first_header           = dtsheader;
-    m_previous_header        = dtsheader;
-    m_get_first_header_later = false;
-
-    if (!m_reader->m_appending)
-      set_headers();
-
-    rerender_track_headers();
-  }
 
   if ((1 < verbose) && (dtsheader != m_previous_header)) {
     mxinfo(Y("DTS header information changed! - New format:\n"));
@@ -155,9 +142,6 @@ dts_packetizer_c::can_connect_to(generic_packetizer_c *src,
   dts_packetizer_c *dsrc = dynamic_cast<dts_packetizer_c *>(src);
   if (NULL == dsrc)
     return CAN_CONNECT_NO_FORMAT;
-
-  if (m_get_first_header_later)
-    return CAN_CONNECT_MAYBE_CODECPRIVATE;
 
   connect_check_a_samplerate(m_first_header.core_sampling_frequency, dsrc->m_first_header.core_sampling_frequency);
   connect_check_a_channels(m_first_header.audio_channels, dsrc->m_first_header.audio_channels);

src/output/p_dts.h
@@ -27,14 +27,13 @@ private:
 
   byte_buffer_c m_packet_buffer;
 
-  bool m_get_first_header_later;
   dts_header_t m_first_header, m_previous_header;
   bool m_skipping_is_normal;
 
   std::deque<int64_t> m_available_timecodes;
 
 public:
-  dts_packetizer_c(generic_reader_c *p_reader, track_info_c &p_ti, const dts_header_t &dts_header, bool get_first_header_later = false);
+  dts_packetizer_c(generic_reader_c *p_reader, track_info_c &p_ti, const dts_header_t &dts_header);
   virtual ~dts_packetizer_c();
 
   virtual int process(packet_cptr packet);

src/input/r_avi.cpp
@@ -681,7 +681,7 @@ avi_reader_c::create_dts_packetizer(int aid) {
 
     AVI_set_audio_position_index(m_avi, audio_position);
 
-    return new dts_packetizer_c(this, m_ti, dtsheader, true);
+    return new dts_packetizer_c(this, m_ti, dtsheader);
 
   } catch (...) {
     mxerror_tid(m_ti.m_fname, aid + 1, Y("Could not find valid DTS headers in this track's first frames.\n"));

src/input/r_matroska.cpp
@@ -1559,7 +1559,7 @@ kax_reader_c::create_dts_audio_packetizer(kax_track_t *t,
     if (-1 == position)
       throw false;
 
-    set_track_packetizer(t, new dts_packetizer_c(this, nti, dtsheader, true));
+    set_track_packetizer(t, new dts_packetizer_c(this, nti, dtsheader));
     show_packetizer_info(t->tnum, t->ptzr_ptr);
 
   } catch (...) {

src/input/r_mpeg_ps.cpp
@@ -1166,7 +1166,7 @@ mpeg_ps_reader_c::create_packetizer(int64_t id) {
       show_packetizer_info(id, PTZR(track->ptzr));
 
     } else if (FOURCC('D', 'T', 'S', ' ') == track->fourcc) {
-      track->ptzr = add_packetizer(new dts_packetizer_c(this, m_ti, track->dts_header, true));
+      track->ptzr = add_packetizer(new dts_packetizer_c(this, m_ti, track->dts_header));
       show_packetizer_info(id, PTZR(track->ptzr));
 
     } else if (FOURCC('T', 'R', 'H', 'D') == track->fourcc) {

commit 465b9718af5707a2d451673175f6baa6f80fa554

Always do search for DTS-HD packets in packetizer
Just not when flushing. Fixes issue introduced in 26565c7.

src/output/p_dts.cpp
@@ -41,7 +41,8 @@ dts_packetizer_c::~dts_packetizer_c() {
 }
 
 unsigned char *
-dts_packetizer_c::get_dts_packet(dts_header_t &dtsheader) {
+dts_packetizer_c::get_dts_packet(dts_header_t &dtsheader,
+                                 bool flushing) {
   if (0 == m_packet_buffer.get_size())
     return NULL;
 
@@ -61,7 +62,7 @@ dts_packetizer_c::get_dts_packet(dts_header_t &dtsheader) {
     buf_size = m_packet_buffer.get_size();
   }
 
-  pos = find_dts_header(buf, buf_size, &dtsheader, !m_first_header.dts_hd);
+  pos = find_dts_header(buf, buf_size, &dtsheader, flushing);
 
   if ((0 > pos) || (static_cast<int>(pos + dtsheader.frame_byte_size) > buf_size))
     return NULL;
@@ -113,11 +114,19 @@ dts_packetizer_c::process(packet_cptr packet) {
   if (-1 != packet->timecode)
     m_available_timecodes.push_back(packet->timecode);
 
+  m_packet_buffer.add(packet->data->get_buffer(), packet->data->get_size());
+
+  process_available_packets(false);
+
+  return FILE_STATUS_MOREDATA;
+}
+
+void
+dts_packetizer_c::process_available_packets(bool flushing) {
   dts_header_t dtsheader;
   unsigned char *dts_packet;
 
-  m_packet_buffer.add(packet->data->get_buffer(), packet->data->get_size());
-  while ((dts_packet = get_dts_packet(dtsheader)) != NULL) {
+  while ((dts_packet = get_dts_packet(dtsheader, flushing)) != NULL) {
     int64_t new_timecode;
     if (!m_available_timecodes.empty()) {
       m_samples_written = 0;
@@ -132,8 +141,11 @@ dts_packetizer_c::process(packet_cptr packet) {
     m_bytes_written   += dtsheader.frame_byte_size;
     m_samples_written += get_dts_packet_length_in_core_samples(&dtsheader);
   }
+}
 
-  return FILE_STATUS_MOREDATA;
+void
+dts_packetizer_c::flush() {
+  process_available_packets(true);
 }
 
 connection_result_e

src/output/p_dts.h
@@ -41,6 +41,7 @@ public:
   virtual void set_skipping_is_normal(bool skipping_is_normal) {
     m_skipping_is_normal = skipping_is_normal;
   }
+  virtual void flush();
 
   virtual const std::string get_format_name(bool translate = true) {
     return translate ? Y("DTS") : "DTS";
@@ -49,7 +50,8 @@ public:
   virtual connection_result_e can_connect_to(generic_packetizer_c *src, std::string &error_message);
 
 private:
-  virtual unsigned char *get_dts_packet(dts_header_t &dts_header);
+  virtual unsigned char *get_dts_packet(dts_header_t &dts_header, bool flushing);
+  virtual void process_available_packets(bool flushing);
 };
 
 #endif // __P_DTS_H