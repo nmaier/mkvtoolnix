03cabbec7920eafbd555f93792a06e892be699b8
Moritz Bunkus <moritz@bunkus.org>
2012-03-08 09:39:55 +0100

    Intentional update
    underflow.mkv is incomplete. libmatroska leaves part of the very last
    block buffer uninitialized as it cannot read the whole buffer. This
    caused frequent failures in this test -- uninitialized memory is
    undefined behaviour. Therefore only use mkvinfo's summary output up to
    but excluding the very last line.

--- tests/test-331read_buffer_underflow.rb
+++ tests/test-331read_buffer_underflow.rb
@@ -1,13 +1,14 @@
 #!/usr/bin/ruby -w
 
-class T_331read_buffer_underflow < Test
-  def description
-    "mkvmerge / read buffer integer underflow on incomplete files"
-  end
+# underflow.mkv is incomplete. libmatroska leaves part of the very
+# last block buffer uninitialized as it cannot read the whole
+# buffer. This caused frequent failures in this test -- uninitialized
+# memory is undefined behaviour. Therefore only use mkvinfo's summary
+# output up to but excluding the very last line.
 
-  def run
-    merge "data/mkv/underflow.mkv"
-    hash_tmp
-  end
+describe "mkvmerge / read buffer integer underflow on incomplete files"
+test "data/mkv/underflow.mkv" do
+  merge "data/mkv/underflow.mkv"
+  info "-s #{tmp}", :output => "| sed -e '$d' > #{tmp}-1"
+  hash_file "#{tmp}-1"
 end
-
