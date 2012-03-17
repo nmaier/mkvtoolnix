dc864014e425b988f39935226f0f1a573b3c403a
Moritz Bunkus <moritz@bunkus.org>
2012-03-17 08:40:43 +0100

    Create unified releases.xml including changes and versions

--- src/tools/changelog_to_xml.rb
+++ src/tools/changelog_to_xml.rb
@@ -2,8 +2,9 @@
 
 require 'rubygems'
 require 'pp'
-gem 'builder', '~> 3.0.0'
 require 'builder'
+require 'rexml/document'
+require 'trollop'
 
 class Array
   def stable_sort_by &block
@@ -12,41 +13,39 @@ class Array
   end
 end
 
-fail "Missing input file name" if ARGV.empty?
+def parse_changelog file_name
+  changelog    = []
+  current_line = []
+  info         = nil
+  version      = 'HEAD'
 
-changelog    = []
-current_line = []
-info         = nil
-version      = 'HEAD'
+  IO.readlines(file_name).each do |line|
+    line = line.chomp.gsub(/^\s+/, '').gsub(/\s+/, ' ')
 
-IO.readlines(ARGV[0]).each do |line|
-  line = line.chomp.gsub(/^\s+/, '').gsub(/\s+/, ' ')
+    if line.empty?
+      if !current_line.empty?
+        version = $1.gsub(/\.+$/, '') if / released? \s+ v? ( [\d\.]+ )/ix.match(current_line[0])
 
-  if line.empty?
-    if !current_line.empty?
-      version = $1 if / released? \s+ v? ( [\d\.]+ )/ix.match(current_line[0])
+        changelog << info.dup.merge({ :content => current_line.join(' '), :version => version })
+        current_line = []
+      end
 
-      changelog << info.dup.merge({ :content => current_line.join(' '), :version => version })
-      current_line = []
-    end
+    elsif / ^ (\d+)-(\d+)-(\d+) \s+ (.+) /x.match(line)
+      info   = { :date => sprintf("%04d-%02d-%02d", $1.to_i, $2.to_i, $3.to_i) }
+      author = $4.gsub(/\s+/, ' ')
+      if /(.+?) \s+ < (.+) >$/x.match(author)
+        info[:author_name]  = $1
+        info[:author_email] = $2
+      else
+        info[:author_name]  = author
+      end
 
-  elsif / ^ (\d+)-(\d+)-(\d+) \s+ (.+) /x.match(line)
-    info   = { :date => sprintf("%04d-%02d-%02d", $1.to_i, $2.to_i, $3.to_i) }
-    author = $4.gsub(/\s+/, ' ')
-    if /(.+?) \s+ < (.+) >$/x.match(author)
-      info[:author_name]  = $1
-      info[:author_email] = $2
     else
-      info[:author_name]  = author
+      current_line << line.gsub(/^\*\s*/, '')
+
     end
-
-  else
-    current_line << line.gsub(/^\*\s*/, '')
-
   end
-end
 
-changelog =
   changelog.
   stable_sort_by { |e| e[:date] }.
   reverse.
@@ -59,18 +58,58 @@ changelog =
     v
   end.
   reverse
+end
 
-xml = Builder::XmlMarkup.new( :indent => 2 )
-xml.instruct! :xml, :version => "1.0", :encoding => "UTF-8"
+def create_all_releases_xml changelog, releases_file_name
+  releases_xml = REXML::Document.new File.new(releases_file_name)
+  builder      = Builder::XmlMarkup.new( :indent => 2 )
 
-xml.releases do |releases|
-  changelog.each do |e|
-    releases.release(:version => e[0]) do |release|
-      e[1].each do |entry|
-        release.change(entry.reject { |key| key == :version })
+  node_to_hash = lambda { |xpath|
+    begin
+      Hash[ *REXML::XPath.first(releases_xml, xpath).elements.collect { |e| [ e.name, e.text ] }.flatten ]
+    rescue
+      Hash.new
+    end
+  }
+
+  builder.instruct! :xml, :version => "1.0", :encoding => "UTF-8"
+
+  xml = builder.tag!('mkvtoolnix-releases') do
+    builder.tag!('latest-source',      node_to_hash.call("/mkvtoolnix-releases/latest-source"))
+    builder.tag!('latest-windows-pre', node_to_hash.call("/mkvtoolnix-releases/latest-windows-pre"))
+
+    changelog.each do |cl_release|
+      attributes = { :version => cl_release[0] }.merge node_to_hash.call("/mkvtoolnix-releases/release[version='#{cl_release[0]}']")
+
+      builder.release attributes do
+        builder.changes do
+          cl_release[1].each do |entry|
+            builder.change(entry[:content], entry.reject { |key| [:version, :content].include? key })
+          end
+        end
       end
     end
   end
+
+  puts xml
 end
 
-puts xml.to_s
+def parse_opts
+  opts = Trollop::options do
+    opt :changelog, "ChangeLog source file",    :type => String, :default => 'ChangeLog'
+    opt :releases,  "releases.xml source file", :type => String, :default => 'releases.xml'
+  end
+
+  Trollop::die :changelog, "must be given and exist" if !opts[:changelog] || !File.exist?(opts[:changelog])
+  Trollop::die :releases,  "must be given and exist" if !opts[:releases]  || !File.exist?(opts[:releases])
+
+  opts
+end
+
+def main
+  opts      = parse_opts
+  changelog = parse_changelog opts[:changelog]
+  create_all_releases_xml changelog, opts[:releases]
+end
+
+main
