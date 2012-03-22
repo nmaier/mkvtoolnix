5a1cc9a4477d28f7acc8986fa4721ec28cec1303
Moritz Bunkus <moritz@bunkus.org>
2012-03-22 13:40:07 +0100

    Additional option for compact XML output

--- src/tools/changelog_to_xml.rb
+++ src/tools/changelog_to_xml.rb
@@ -6,13 +6,13 @@ require 'builder'
 require 'rexml/document'
 require 'trollop'
 
-def parse_changelog file_name
+def parse_changelog
   changelog    = []
   current_line = []
   info         = nil
   version      = 'HEAD'
 
-  IO.readlines(file_name).each do |line|
+  IO.readlines($opts[:changelog]).each do |line|
     line = line.chomp.gsub(/^\s+/, '').gsub(/\s+/, ' ')
 
     if line.empty?
@@ -42,9 +42,9 @@ def parse_changelog file_name
   changelog.chunk { |e| e[:version] }.collect { |e| e }
 end
 
-def create_all_releases_xml changelog, releases_file_name
-  releases_xml = REXML::Document.new File.new(releases_file_name)
-  builder      = Builder::XmlMarkup.new( :indent => 2 )
+def create_all_releases_xml changelog
+  releases_xml = REXML::Document.new File.new($opts[:releases])
+  builder      = Builder::XmlMarkup.new( :indent => $opts[:noindent] ? 0 : 2 )
 
   node_to_hash = lambda { |xpath|
     begin
@@ -61,7 +61,6 @@ def create_all_releases_xml changelog, releases_file_name
     builder.tag!('latest-windows-pre', node_to_hash.call("/mkvtoolnix-releases/latest-windows-pre"))
 
     changelog.each do |cl_release|
-      next if cl_release[0] != "4.8.0"
       attributes = { "version" => cl_release[0]}.merge node_to_hash.call("/mkvtoolnix-releases/release[version='#{cl_release[0]}']")
 
       builder.release attributes do
@@ -81,6 +80,7 @@ def parse_opts
   opts = Trollop::options do
     opt :changelog, "ChangeLog source file",    :type => String, :default => 'ChangeLog'
     opt :releases,  "releases.xml source file", :type => String, :default => 'releases.xml'
+    opt :noindent,  "do not indent the XML"
   end
 
   Trollop::die :changelog, "must be given and exist" if !opts[:changelog] || !File.exist?(opts[:changelog])
@@ -90,9 +90,8 @@ def parse_opts
 end
 
 def main
-  opts      = parse_opts
-  changelog = parse_changelog opts[:changelog]
-  create_all_releases_xml changelog, opts[:releases]
+  $opts = parse_opts
+  create_all_releases_xml parse_changelog
 end
 
 main
