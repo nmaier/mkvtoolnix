7e4204f5f9c2d7357f4b8f687433639a2f55b1cf
Moritz Bunkus <moritz@bunkus.org>
2012-08-05 12:37:16 +0200

    Add std::ostream << operator for mtx::exception
    Also refactoring.

--- src/common/error.h
+++ src/common/error.h
@@ -11,29 +11,40 @@
    Written by Moritz Bunkus <moritz@bunkus.org>.
 */
 
-#ifndef __MTX_COMMON_ERROR_H
-#define __MTX_COMMON_ERROR_H
+#ifndef MTX_COMMON_ERROR_H
+#define MTX_COMMON_ERROR_H
 
 #include "common/common_pch.h"
 
+#include <ostream>
+
 namespace mtx {
-  class exception: public std::exception {
-  public:
-    virtual const char *what() const throw() {
-      return "unspecified MKVToolNix error";
-    }
 
-    virtual std::string error() const throw() {
-      return what();
-    }
-  };
+class exception: public std::exception {
+public:
+  virtual const char *what() const throw() {
+    return "unspecified MKVToolNix error";
+  }
 
-  class invalid_parameter_x: public exception {
-  public:
-    virtual const char *what() const throw() {
-      return "invalid parameter in function call";
-    }
-  };
+  virtual std::string error() const throw() {
+    return what();
+  }
+};
+
+class invalid_parameter_x: public exception {
+public:
+  virtual const char *what() const throw() {
+    return "invalid parameter in function call";
+  }
+};
+
+inline std::ostream &
+operator <<(std::ostream &out,
+            exception const &ex) {
+  out << ex.what();
+  return out;
 }
 
-#endif // __MTX_COMMON_ERROR_H
+}
+
+#endif // MTX_COMMON_ERROR_H
