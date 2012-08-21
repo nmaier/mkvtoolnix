284d2dc76baeac60d13b191db8eb914596885560
Moritz Bunkus <moritz@bunkus.org>
2012-08-21 11:22:48 +0200

    Move dump to util.cpp; implement EBML structure comparator class & assertions

--- tests/unit/util.h
+++ tests/unit/util.h
@@ -16,69 +16,38 @@
 
 #include "common/common_pch.h"
 
-#include <iostream>
+#include <ebml/EbmlElement.h>
 
-#include <ebml/EbmlBinary.h>
-#include <ebml/EbmlDate.h>
-#include <ebml/EbmlFloat.h>
-#include <ebml/EbmlSInteger.h>
-#include <ebml/EbmlString.h>
-#include <ebml/EbmlUInteger.h>
-#include <ebml/EbmlUnicodeString.h>
-#include <ebml/EbmlVoid.h>
+#include "gtest/gtest.h"
+#include "common/strings/formatting.h"
+
+#define ASSERT_EBML_EQ(a, b) ASSERT_PRED_FORMAT2(EbmlEquals, (a), (b))
+#define EXPECT_EBML_EQ(a, b) EXPECT_PRED_FORMAT2(EbmlEquals, (a), (b))
 
 namespace mtxut {
 
 using namespace libebml;
 
-void
-dump(EbmlElement *element,
-     bool with_values = false,
-     unsigned int level = 0) {
-  std::string indent_str, value_str;
-  size_t i;
+void dump(EbmlElement *element, bool with_values = false, unsigned int level = 0);
 
-  for (i = 1; i <= level; ++i)
-    indent_str += " ";
+::testing::AssertionResult EbmlEquals(char const *a_expr, char const *b_expr, EbmlElement &a, EbmlElement &b);
 
-  if (with_values) {
-    if (dynamic_cast<EbmlUInteger *>(element))
-      value_str = to_string(uint64(*static_cast<EbmlUInteger *>(element)));
+class ebml_equals_c {
+private:
+  std::vector<std::string> m_path;
+  std::string m_error;
 
-    else if (dynamic_cast<EbmlSInteger *>(element))
-      value_str = to_string(int64(*static_cast<EbmlSInteger *>(element)));
+public:
+  ebml_equals_c();
 
-    else if (dynamic_cast<EbmlFloat *>(element))
-      value_str = to_string(double(*static_cast<EbmlFloat *>(element)));
+  bool compare_impl(EbmlElement &a, EbmlElement &b);
+  std::string get_error() const;
+  bool set_error(std::string const &error, EbmlElement *cmp = nullptr);
+  bool set_error(boost::format const &error, EbmlElement *cmp = nullptr);
 
-    else if (dynamic_cast<EbmlUnicodeString *>(element))
-      value_str = UTFstring_to_cstrutf8(UTFstring(*static_cast<EbmlUnicodeString *>(element)));
-
-    else if (dynamic_cast<EbmlString *>(element))
-      value_str = std::string(*static_cast<EbmlString *>(element));
-
-    else if (dynamic_cast<EbmlDate *>(element))
-      value_str = to_string(static_cast<EbmlDate *>(element)->GetEpochDate());
-
-    else
-      value_str = (boost::format("(type: %1%)") %
-                   (  dynamic_cast<EbmlBinary *>(element) ? "binary"
-                    : dynamic_cast<EbmlMaster *>(element) ? "master"
-                    : dynamic_cast<EbmlVoid *>(element)   ? "void"
-                    :                                       "unknown")).str();
-
-    value_str = " " + value_str;
-  }
-
-  std::cout << indent_str << EBML_NAME(element) << value_str << std::endl;
-
-  EbmlMaster *master = dynamic_cast<EbmlMaster *>(element);
-  if (!master)
-    return;
-
-  for (auto el : *master)
-    dump(el, with_values, level + 1);
-}
+public:
+  static bool check(EbmlElement &a, EbmlElement &b, std::string &error);
+};
 
 }
 
