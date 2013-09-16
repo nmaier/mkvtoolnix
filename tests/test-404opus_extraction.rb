4e2cd32c31cb90430715d4c4a5eb0ca1bca07749
Moritz Bunkus <moritz@bunkus.org>
2013-09-16 21:20:41 +0200

    Opus extraction test result update

--- tests/test-404opus_extraction.rb
+++ tests/test-404opus_extraction.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/ruby -w
 
-describe "mkvmerge / extract Opus from MKA in final mode"
+describe "mkvextract / extract Opus from MKA in final mode"
 test "extraction" do
   extract "data/opus/v-opus.mka", 0 => tmp
   hash_tmp
