467c84afac996d5ef16119f3b8bfeb4adb48f4a2
Moritz Bunkus <moritz@bunkus.org>
2013-12-27 19:31:49 +0100

    common: new codec_c class for codec identification

--- src/common/codec.h
+++ src/common/codec.h
@@ -14,6 +14,12 @@
 #ifndef MTX_COMMON_CODEC_H
 #define MTX_COMMON_CODEC_H
 
+#include "common/common_pch.h"
+
+#include <ostream>
+
+#include "common/fourcc.h"
+
 // see http://www.matroska.org/technical/specs/codecid/index.html
 
 #define MKV_A_AAC_2MAIN  "A_AAC/MPEG2/MAIN"
@@ -80,5 +86,129 @@
 #define MKV_S_HDMV_PGS   "S_HDMV/PGS"
 
 #define MKV_B_VOBBTN     "B_VOBBTN"
+
+enum codec_type_e {
+    CT_UNKNOWN  = 0
+  , CT_V_MPEG12 = 0x1000
+  , CT_V_MPEG4_P2
+  , CT_V_MPEG4_P10
+  , CT_V_REAL
+  , CT_V_THEORA
+  , CT_V_DIRAC
+  , CT_V_VP8
+  , CT_V_VP9
+  , CT_V_SVQ
+  , CT_V_VC1
+
+  , CT_A_AAC = 0x2000
+  , CT_A_AC3
+  , CT_A_ALAC
+  , CT_A_DTS
+  , CT_A_MP2
+  , CT_A_MP3
+  , CT_A_PCM
+  , CT_A_VORBIS
+  , CT_A_OPUS
+  , CT_A_QDMC
+  , CT_A_FLAC
+  , CT_A_MLP
+  , CT_A_TRUEHD
+  , CT_A_TTA
+  , CT_A_WAVPACK4
+
+  , CT_S_SRT = 0x3000
+  , CT_S_SSA_ASS
+  , CT_S_USF
+  , CT_S_VOBSUB
+  , CT_S_KATE
+  , CT_S_PGS
+
+  , CT_B_VOBBTN = 0x4000
+};
+
+class codec_c {
+private:
+  static std::vector<codec_c> ms_codecs;
+
+protected:
+  boost::regex m_match_re;
+  std::string m_name;
+  codec_type_e m_type;
+  std::vector<uint16_t> m_audio_formats;
+
+public:
+  codec_c()
+    : m_type{CT_UNKNOWN}
+  {
+  }
+
+  codec_c(std::string const &name, codec_type_e type, std::string const &match_re, uint16_t audio_format = 0u)
+    : m_match_re{(boost::format("(?:%1%)") % match_re).str(), boost::regex::perl | boost::regex::icase}
+    , m_name{name}
+    , m_type{type}
+  {
+    if (audio_format)
+      m_audio_formats.push_back(audio_format);
+  }
+
+  codec_c(std::string const &name, codec_type_e type, std::string const &match_re, std::vector<uint16_t> audio_formats)
+    : m_match_re{(boost::format("(?:%1%)") % match_re).str(), boost::regex::perl | boost::regex::icase}
+    , m_name{name}
+    , m_type{type}
+    , m_audio_formats{audio_formats}
+  {
+  }
+
+  bool matches(std::string const &fourcc_or_codec_id) const {
+    return boost::regex_match(fourcc_or_codec_id, m_match_re);
+  }
+
+  bool valid() const {
+    return m_type != CT_UNKNOWN;
+  }
+
+  operator bool() const {
+    return valid();
+  }
+
+  bool is(codec_type_e type) const {
+    return type == m_type;
+  }
+
+  std::string const get_name(std::string fallback = "") const {
+    if (!valid())
+      return fallback;
+    return m_name;
+  }
+
+  codec_type_e get_type() const {
+    return m_type;
+  }
+
+private:
+  static void initialize();
+
+public:                         // static
+  static codec_c const look_up(std::string const &fourcc_or_codec_id);
+  static codec_c const look_up(char const *fourcc_or_codec_id);
+  static codec_c const look_up(fourcc_c const &fourcc);
+  static codec_c const look_up(codec_type_e type);
+  static codec_c const look_up_audio_format(uint16_t audio_format);
+  static codec_c const look_up_object_type_id(unsigned int object_type_id);
+
+  static std::string const get_name(std::string const &fourcc_or_codec_id, std::string const &fallback);
+  static std::string const get_name(codec_type_e type, std::string const &fallback);
+};
+
+inline std::ostream &
+operator <<(std::ostream &out,
+            codec_c const &codec) {
+  if (codec)
+    out << (boost::format("%1% (0x%|2$04x|)") % codec.get_name() % codec.get_type());
+  else
+    out << "<invalid-codec>";
+
+  return out;
+}
 
 #endif // MTX_COMMON_CODEC_H
