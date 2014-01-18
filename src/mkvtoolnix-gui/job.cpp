fec6bf59d8118a473df226a8b4392d5a19f7e6ca
Moritz Bunkus <moritz@bunkus.org>
2014-01-18 13:38:48 +0100

    GUI: implement basis of Job and MuxJob classes

---
+++ src/mkvtoolnix-gui/job.cpp
@@ -0,0 +1,104 @@
+#include "common/common_pch.h"
+
+#include "common/qt.h"
+#include "mkvtoolnix-gui/job.h"
+
+uint64_t Job::ms_next_id = 0;
+
+Job::Job(Status status)
+  : m_id{ms_next_id++}
+  , m_status{status}
+  , m_progress{std::numeric_limits<unsigned int>::max()}
+  , m_exitCode{std::numeric_limits<unsigned int>::max()}
+{
+}
+
+Job::~Job() {
+}
+
+void
+Job::action(std::function<void()> code) {
+  m_mutex.lock();
+  code();
+  m_mutex.unlock();
+}
+
+void
+Job::setStatus(Status status) {
+  action([this,status]() {
+      if (status == m_status) {
+        m_status = status;
+        emit statusChanged(m_id, m_status);
+      }
+    });
+}
+
+void
+Job::setProgress(unsigned int progress) {
+  action([this,progress]() {
+      if (progress == m_progress) {
+        m_progress = progress;
+        emit progressChanged(m_id, m_progress);
+      }
+    });
+}
+
+// ------------------------------------------------------------
+
+MuxJob::MuxJob(Status status)
+  : Job{status}
+{
+}
+
+MuxJob::~MuxJob() {
+}
+
+void
+MuxJob::abort() {
+  if (m_thread)
+    m_thread->abort();
+}
+
+void
+MuxJob::start() {
+  assert(!m_thread);
+  m_thread = new MuxJobThread{this};
+  m_thread->start();
+}
+
+void
+MuxJob::threadFinished() {
+  m_thread = nullptr;
+}
+
+// ------------------------------------------------------------
+
+MuxJobThread::MuxJobThread(MuxJob *job)
+  : QThread{}
+  , m_aborted{false}
+{
+  connect(this, SIGNAL(progressChanged(unsigned int)), job, SLOT(setProgress(unsigned int)));
+  connect(this, SIGNAL(statusChanged(unsigned int)),   job, SLOT(setStatus(unsigned int)));
+  connect(this, SIGNAL(finished()),                    job, SLOT(threadFinished()));
+}
+
+void
+MuxJobThread::abort() {
+  m_aborted = true;
+}
+
+void
+MuxJobThread::run() {
+  m_aborted = false;
+  emit progressChanged(0);
+  emit statusChanged(Job::Running);
+
+  // TODO: MuxJobThread::run
+  for (int i = 0; i < 10; i++) {
+    msleep(500);
+    emit progressChanged(i * 10);
+  }
+
+  emit progressChanged(100);
+  emit statusChanged(Job::DoneOk);
+}
---
+++ src/mkvtoolnix-gui/job.h
@@ -0,0 +1,93 @@
+#ifndef MTX_MKVTOOLNIX_GUI_APP_H
+#define MTX_MKVTOOLNIX_GUI_APP_H
+
+#include "common/common_pch.h"
+
+#include <QObject>
+#include <QMutex>
+#include <QString>
+#include <QStringList>
+#include <QThread>
+
+class Job: public QObject {
+  Q_OBJECT;
+public:
+  enum Status {
+    PendingManual,
+    PendingAuto,
+    Running,
+    DoneOk,
+    DoneWarnings,
+    Failed,
+    Aborted,
+    Disabled,
+  };
+
+private:
+  static uint64_t ms_next_id;
+
+public:
+  uint64_t m_id;
+  Status m_status;
+  QString m_description;
+  QStringList m_output, m_warnings, m_errors;
+  unsigned int m_progress, m_exitCode;
+
+  QMutex m_mutex;
+
+public:
+  Job(Status status = PendingManual);
+  virtual ~Job();
+
+  void action(std::function<void()> code);
+
+  virtual void abort() = 0;
+  virtual void start() = 0;
+
+public slots:
+  void setStatus(Status status);
+  void setProgress(unsigned int progress);
+
+signals:
+  void statusChanged(uint64_t id, Status status);
+  void progressChanged(uint64_t id, unsigned int progress);
+};
+typedef std::shared_ptr<Job> JobPtr;
+
+class MuxJob;
+
+class MuxJobThread: public QThread {
+  Q_OBJECT;
+protected:
+  volatile bool m_aborted;
+
+public:
+  MuxJobThread(MuxJob *job);
+
+  void abort();
+
+protected:
+  virtual void run();
+
+signals:
+  void progressChanged(unsigned int progress);
+  void statusChanged(Job::Status status);
+};
+
+class MuxJob: public Job {
+  Q_OBJECT;
+protected:
+  MuxJobThread *m_thread;
+
+public:
+  MuxJob(Status status);
+  virtual ~MuxJob();
+
+  virtual void abort();
+  virtual void start();
+
+public slots:
+  void threadFinished();
+};
+
+#endif  // MTX_MKVTOOLNIX_GUI_APP_H
